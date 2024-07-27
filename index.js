1076714a0390e23a35c0296670f16fc442934b50
sebas-mod <luisrojas30921@gmail.com>
2024-07-26 23:14:51 -0300

    Update index.js

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ import cfonts from 'cfonts';
 import { createInterface } from 'readline';
 import yargs from 'yargs';
 import chalk from 'chalk';
-console.log('\n✰ Iniciando LuffyBot ✰');
+console.log('\n✰ Iniciando tanjiro ✰');
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const require = createRequire(__dirname);
 const { name, description, author, version } = require(join(__dirname, './package.json'));
 const { say } = cfonts;
 const rl = createInterface(process.stdin, process.stdout);
-say('Luffy\nBot-MD', {
+say('tanjiro\nBot', {
 font: 'block',
 align: 'center',
 colors: ['white']
@@ -23,7 +23,7 @@ font: 'chrome',
 align: 'center',
 colors: ['red']
 });
-say(`Developed By • OfcDiego`, {
+say(`Developed By • sebas-mod`, {
 font: 'console',
 align: 'center',
 colors: ['yellow']
