a2049c73e2078dbb972bd9e649e37d9f45bd386a
sebas-mod <luisrojas30921@gmail.com>
2024-07-31 16:43:22 -0300

    Update owner-salir.js

--- plugins/owner-salir.js
+++ plugins/owner-salir.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, text, command }) => {
 let id = text ? text : m.chat  
 let chat = global.db.data.chats[m.chat]
 chat.welcome = false
-await conn.reply(id, `🚩 *tanjirobot* Abandona El Grupo, hasta la proxima negros👋`) 
+await conn.reply(id, `🚩 *tanjiro-bot* Abandona El Grupo, hasta la proxima negros👋`) 
 await conn.groupLeave(id)
 try {  
 chat.welcome = true
