9a7fab37d967e40ec097b9d9bdde85f841cc1af9
sebas-mod <luisrojas30921@gmail.com>
2024-07-31 12:19:20 -0300

    Update gc-setbye.js

--- plugins/gc-setbye.js
+++ plugins/gc-setbye.js
@@ -3,7 +3,7 @@
 let handler = async (m, { conn, text, isROwner, isOwner }) => {
    if (text) {
      global.db.data.chats[m.chat].sBye = text
-     m.reply('✅ mensaje de despedida agregado exitosamente')
+     m.reply('✅ mensaje de despedida configurado exitosamente')
    } else throw `✳️ agregue el mensaje\n@user (mention)`
 }
 handler.help = ['setbye <text>']
