2acacb9f69d059555d1e41d6024e5f1b793d5f31
sebas-mod <luisrojas30921@gmail.com>
2024-07-31 13:07:52 -0300

    Update fun-ttt.js

--- plugins/fun-ttt.js
+++ plugins/fun-ttt.js
@@ -3,7 +3,7 @@ import TicTacToe from '../lib/tictactoe.js'
 let handler = async (m, { conn, usedPrefix, command, text }) => {
      conn.game = conn.game ? conn.game : {}
      if (Object.values(conn.game).find(room => room.id.startsWith('tictactoe') && [room.game.playerX, room.game.playerO].includes(m.sender))) throw `✳️ aun estas jugando en otra sala para cerrar pon: *${usedPrefix}delttt*`
-     if (!text) throw ` ✳️ dale un nombre  la sala`
+     if (!text) throw ` ✳️ dale un nombre a la sala`
      let room = Object.values(conn.game).find(room => room.state === 'WAITING' && (text ? room.name === text : true))
      // m.reply('[WIP Feature]')
      if (room) {
