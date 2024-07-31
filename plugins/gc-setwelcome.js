d0856dd113586dd8381d391e8fc24cb36b2567ae
sebas-mod <luisrojas30921@gmail.com>
2024-07-31 12:08:22 -0300

    Update gc-setwelcome.js

--- plugins/gc-setwelcome.js
+++ plugins/gc-setwelcome.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, text, isROwner, isOwner }) => {
 if (text) {
 global.db.data.chats[m.chat].sWelcome = text
 m.reply('*[❗] mensaje de bienvenida configurado exitosamente *')
-} else throw ` *[❗] ingrese el mensaje de bienvenida que quieres agregar, 𝚄𝚂𝙴:*\n*- @user (mención)*\n*- @group (nombre de grupo)*\n*- @desc (description de grupo)*`
+} else throw ` *[❗] ingrese el mensaje de bienvenida que quieres agregar:*\n*- @user (mención)*\n*- @group (nombre de grupo)*\n*- @desc (description de grupo)*`
 }
 handler.help = ['setwelcome <text>']
 handler.tags = ['grupo']
