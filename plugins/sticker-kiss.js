3e8d710e1d1627d5ae6370a841ad36c95efdb5ed
sebas-mod <luisrojas30921@gmail.com>
2024-08-02 18:50:34 -0300

    Actualizar sticker-kiss.js

--- plugins/sticker-kiss.js
+++ plugins/sticker-kiss.js
@@ -23,7 +23,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
 }
 
 handler.help = ['kiss @tag']
-handler.tags = ['rnime']
+handler.tags = ['fun']
 handler.command = /^(kiss|beso)$/i
 handler.diamond = true
 handler.group = true
