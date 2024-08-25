837db0bab020529f226ab60897670e832001612d
sebas-mod <luisrojas30921@gmail.com>
2024-08-24 21:35:16 -0300

    Actualizar gc-todos2.js

--- plugins/gc-todos2.js
+++ plugins/gc-todos2.js
@@ -10,13 +10,13 @@ const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, com
   }
   const pesan = args.join` `;
   const oi = `${pesan}`;
-  let teks = `𝙏𝘼𝙉𝙅𝙄𝙍𝙊 𝘽𝙊𝙏\n\n*𝙋𝙖𝙧𝙩𝙞𝙘𝙞𝙥𝙖𝙣𝙩𝙚𝙨:  ${participants.length}* ${oi}\n\n┌──│𝙍𝙀𝙑𝙄𝙑𝘼𝙉 𝙁𝘼𝙉𝙏𝘼𝙎𝙈𝘼𝙎👻\n`;
+  let teks = `𝙏𝘼𝙉𝙅𝙄𝙍𝙊 𝘽𝙊𝙏👿\n\n*𝙋𝙖𝙧𝙩𝙞𝙘𝙞𝙥𝙖𝙣𝙩𝙚𝙨:  ${participants.length}* ${oi}\n\n┌──│𝙍𝙀𝙑𝙄𝙑𝘼𝙉 𝙁𝘼𝙉𝙏𝘼𝙎𝙈𝘼𝙎👻\n`;
   for (const mem of participants) {
     teks += `│ ♛►@${mem.id.split('@')[0]}\n`;
   }
-  teks += `└───────│ ♛
+  teks += `└───────│♛
 
-by:𝙎𝙚𝙗𝙖𝙨 𝙈𝙤𝙙`;
+by: 𝙎𝙚𝙗𝙖𝙨 𝙈𝙤𝙙`;
   conn.sendMessage(m.chat, {text: teks, mentions: participants.map((a) => a.id)} );
 };
 handler.help = ['invocar <mesaje>', ' <mesaje>'];
